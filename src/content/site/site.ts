006e8184fff6998c6ff25d91077eb288a7a5de29
sanderalex1 <oleksandr.kodehode@gmail.com>
2026-04-28 23:09:45 +0200

    added CV

--- src/content/site/site.ts
+++ src/content/site/site.ts
@@ -1,14 +1,13 @@
 // Top-level site config — the bits you'll change most often.
-
+import cv from "../../assets/cv/CV.pdf";
 import type { SiteConfig } from "./siteTypes";
-
 export const site: SiteConfig = {
   email: "alex.myrhorodskyi@gmail.com",
 
   social: {
     github: "https://github.com/sanderalex1",
     linkedin: "https://linkedin.com/in/alex-myrhorodskyi-b688711b7/",
-    readcv: "https://read.cv/janedoe",
+    readcv: cv,
   },
   copyrightYear: 2026,
   builtYear: 2026,
