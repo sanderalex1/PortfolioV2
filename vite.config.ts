505bda71cac6b12bf54004ba265c426117553e8b
sanderalex1 <oleksandr.kodehode@gmail.com>
2026-04-28 20:31:57 +0200

    feat: update base path in vite.config.ts to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sanderalex1/",
+  base: "/PortfolioV2/",
   plugins: [react()],
 });
